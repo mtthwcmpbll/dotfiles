a70d7aaee2c774989903354ec5256511d953e98b
Matthew Campbell <mjc4648@gmail.com>
2016-12-20 10:58:17 -0500

    Fixed some issues with the symlinks and added some standard tools

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
-ln -sv "~/dotfiles/.aliases" ~
-ln -sv "~/dotfiles/.bash_profile" ~
-ln -sv "~/dotfiles/.bashrc" ~
-ln -sv "~/dotfiles/.env" ~
-ln -sv "~/dotfiles/.gitconfig" ~
-ln -sv "~/dotfiles/.gitignore_global" ~
-ln -sv "~/dotfiles/.tools" ~
-ln -sv "~/dotfiles/.zshrc" ~
+# Link dotfiles into the user's home directory
+ln -svi "$HOME/dotfiles/.aliases" ~
+ln -svi "$HOME/dotfiles/.bash_profile" ~
+ln -svi "$HOME/dotfiles/.bashrc" ~
+ln -svi "$HOME/dotfiles/.env" ~
+ln -svi "$HOME/dotfiles/.gitconfig" ~
+ln -svi "$HOME/dotfiles/.gitignore_global" ~
+ln -svi "$HOME/dotfiles/.tools" ~
+ln -svi "$HOME/dotfiles/.zshrc" ~
+
+# Install some standard tools with Homebrew
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install bash-completion wget git mercurial maven
+brew install go || true
