0e9e6c872052ec3f69dd8e923fd5c5eab05497b7
Matthew Campbell <mjc4648@gmail.com>
2016-12-22 16:20:44 -0500

    Readded working cf autocomplete plugin, and update install script to make sure submodules are updated.

--- install.sh
+++ install.sh
@@ -1,17 +1,23 @@
+BASEDIR=$( cd "$(dirname "$0")" ; pwd -P )
+
 # Link dotfiles into the user's home directory
-ln -svi "$HOME/dotfiles/.aliases" ~
-ln -svi "$HOME/dotfiles/.bash_profile" ~
-ln -svi "$HOME/dotfiles/.bashrc" ~
-ln -svi "$HOME/dotfiles/.env" ~
-ln -svi "$HOME/dotfiles/.gitconfig" ~
-ln -svi "$HOME/dotfiles/.gitignore_global" ~
-ln -svi "$HOME/dotfiles/.tools" ~
-ln -svi "$HOME/dotfiles/.zshrc" ~
+ln -svi "$BASEDIR/.aliases" ~
+ln -svi "$BASEDIR/.bash_profile" ~
+ln -svi "$BASEDIR/.bashrc" ~
+ln -svi "$BASEDIR/.env" ~
+ln -svi "$BASEDIR/.gitconfig" ~
+ln -svi "$BASEDIR/.gitignore_global" ~
+ln -svi "$BASEDIR/.tools" ~
+ln -svi "$BASEDIR/.zshrc" ~
 
 # Install some standard tools with Homebrew
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
 brew tap cloudfoundry/tap
-brew install bash-completion wget git mercurial maven cf-cli rbenv
+brew install bash-completion wget git mercurial maven rbenv
+brew install cf-cli || true
 brew install go || true
+
+git -C $BASEDIR submodule init
+git -C $BASEDIR submodule update
